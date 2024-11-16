e57c5df17a5fbd8fc7eb68d5d464ecc045a861db
JamesL2004 <James002>
2024-11-16 15:47:57 -0500

    Finished main code for regression model

--- jdlynch_FinalProject.py
+++ jdlynch_FinalProject.py
@@ -1,6 +1,7 @@
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
 def runRegressionModel(trainX, testX, trainY, testY):
@@ -24,13 +25,22 @@ def runRegressionModel(trainX, testX, trainY, testY):
     return 0
 
 df = pd.read_csv("student_sleep_patterns.csv", delimiter=",")
+scaler = StandardScaler()
 
 gender_mapping = {'Male': 1, 'Female': 2, 'Other': 3}
 df['Gender_numeric'] = df['Gender'].map(gender_mapping)
 
-coeffients = ["Gender_numeric"]
+year_mapping = {'1st Year': 1, '2nd Year': 2, '3rd Year': 3, '4th Year': 4}
+df['Year_numeric'] = df['University_Year'].map(year_mapping)
+
+coeffients = ["Study_Hours", "Gender_numeric", "Year_numeric", "Caffeine_Intake", "Physical_Activity"]
 Y = "Sleep_Quality"
 
 sleep_X = df[coeffients]
 sleep_Y = df[Y]
 
+sleep_X = scaler.fit_transform(sleep_X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(sleep_X, sleep_Y, test_size=0.2)
+
+runRegressionModel(X_train, X_test, Y_train, Y_test)
