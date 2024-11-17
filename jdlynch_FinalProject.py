60f99aea5b472fe358d9f6cc243fe1ad7f12a07c
JamesL2004 <James002>
2024-11-17 12:36:39 -0500

    Tried different SL classifiers to see results

--- jdlynch_FinalProject.py
+++ jdlynch_FinalProject.py
@@ -5,6 +5,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,7 +25,7 @@ df['Gender_numeric'] = df['Gender'].map(gender_mapping)
 year_mapping = {'1st Year': 1, '2nd Year': 2, '3rd Year': 3, '4th Year': 4}
 df['Year_numeric'] = df['University_Year'].map(year_mapping)
 
-coeffients = ["Study_Hours", "Gender_numeric", "Year_numeric", "Caffeine_Intake", "Physical_Activity"]
+coeffients = ["Caffeine_Intake", "Age", "Year_numeric", "Screen_Time", "Sleep_Duration", "Physical_Activity"]
 Y = "Sleep_Quality"
 
 sleep_X = df[coeffients]
@@ -30,15 +36,36 @@ X_train, X_test, Y_train, Y_test = train_test_split(sleep_X, sleep_Y, test_size=
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-degree = 2
-poly_regression_model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
-poly_regression_model.fit(X_train_scaled, Y_train)
+classifierKNN = KNeighborsClassifier(n_neighbors=5)
+classifierKNN.fit(X_train_scaled, Y_train)
+otherClassifierTestPred = classifierKNN.predict(X_test_scaled)
+npYtest = np.array(Y_test)
+print("K-Nearest Neighbour " + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
 
-y_pred = poly_regression_model.predict(X_test_scaled)
+classifierRndForest = RandomForestClassifier(verbose=True)
+classifierRndForest.fit(X_train_scaled, Y_train)
+otherClassifierTestPred = classifierRndForest.predict(X_test_scaled)
+npYtest = np.array(Y_test)
+print("Random Forest " + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
 
-print("MSE: %.2f" % mean_squared_error(Y_test, y_pred))
-print("R^2: %.2f" % r2_score(Y_test, y_pred))
+classifierNB = GaussianNB()
+classifierNB.fit(X_train_scaled, Y_train)
+otherClassifierTestPred = classifierNB.predict(X_test_scaled)
+npYtest = np.array(Y_test)
+print("Gaussian NB" + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
 
-sns.histplot(df['Sleep_Quality'], kde=True)
-plt.title("Distribution of Sleep Quality")
-plt.show()
+classifierNB = svm.LinearSVC()
+classifierNB.fit(X_train_scaled, Y_train)
+otherClassifierTestPred = classifierRndForest.predict(X_test_scaled)
+npYtest = np.array(Y_test)
+print("SVM " + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
+
+#degree = 2
+#poly_regression_model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+#poly_regression_model.fit(X_train_scaled, Y_train)
+
+#y_pred = poly_regression_model.predict(X_test_scaled)
+
+#print("MSE: %.2f" % mean_squared_error(Y_test, y_pred))
+#print("R^2: %.2f" % r2_score(Y_test, y_pred))
+
