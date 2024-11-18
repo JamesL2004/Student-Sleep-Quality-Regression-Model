5c4dcc82bca412ffdf8280fc78a5d71de9210539
JamesL2004 <James002>
2024-11-18 12:14:31 -0500

    Changed to a better dataset an instantly got better results.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This github project is the python code for a regession model that is trying to predict a students sleep_quality based on outside factors like caffiene intake, study hours, etc.
 
-The dataset used: https://www.kaggle.com/datasets/arsalanjamal002/student-sleep-patterns/data
+The dataset used: https://www.kaggle.com/datasets/uom190346a/sleep-health-and-lifestyle-dataset
--- jdlynch_FinalProject.py
+++ jdlynch_FinalProject.py
@@ -1,8 +1,8 @@
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn import svm
@@ -29,6 +29,16 @@ df = pd.read_csv("student_sleep_patterns.csv", delimiter=",")
 df = df.dropna()
 scaler = StandardScaler()
 
+df2 = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv", delimiter=",")
+print(df2.columns)
+
+gender = {'Male': 1, 'Female': 2}
+df2['gender_numeric'] = df2['Gender'].map(gender)
+occupation_mapping = {'Nurse': 1, 'Sales Representative': 2, 'Salesperson': 3, 'Scientist': 4, 'Software Engineer': 5, 'Teacher': 6, 'Accountant': 7, 'Doctor': 8, 'Engineer': 9, 'Lawyer': 10, 'Manager': 11}
+disorder_mapping = {'Insomnia': 1, 'None': 2, 'Sleep Apnea': 3}
+df2['disorder_numeric'] = df2["Occupation"].apply(occupation_mapping)
+bmi_mapping = {'Normal': 1, 'Normal Weight': 2, 'Obese': 3, 'Overweight': 4}
+
 gender_mapping = {'Male': 1, 'Female': 2, 'Other': 3}
 df['Gender_numeric'] = df['Gender'].map(gender_mapping)
 
@@ -38,20 +48,22 @@ df['Year_numeric'] = df['University_Year'].map(year_mapping)
 df['Sleep_Quality_Category'] = pd.cut(df['Sleep_Quality'], bins=[0, 2, 4, 6, 8, 10], labels=['Poor', 'Fair', 'Good', 'Very Good', 'Excellent'])
 df['percOfStudy'] = df.apply(lambda row: TotalPercentOfStudying(row['Sleep_Duration'], row['Study_Hours']), axis=1)
 
-coeffients = ["Caffeine_Intake", "Age", "Year_numeric", "Screen_Time", "Sleep_Duration", "Physical_Activity", "Gender_numeric", "percOfStudy"]
-Y = "Sleep_Quality_Category"
+coeffients = ["Sleep Duration", "Heart Rate", "Daily Steps", "gender_numeric", "occupation_mapping"]
+Y = "Quality of Sleep"
 
-sleep_X = df[coeffients]
-sleep_Y = df[Y]
+#df[coeffients] = (df[coeffients] - df[coeffients].mean()) / df[coeffients].std()
+
+sleep_X = df2[coeffients]
+sleep_Y = df2[Y]
 
 X_train, X_test, Y_train, Y_test = train_test_split(sleep_X, sleep_Y, test_size=0.2)
 
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-classifierKNN = KNeighborsClassifier(n_neighbors=5)
-classifierKNN.fit(X_train_scaled, Y_train)
-otherClassifierTestPred = classifierKNN.predict(X_test_scaled)
+classifierKNN = KNeighborsClassifier(n_neighbors=3)
+classifierKNN.fit(X_train, Y_train)
+otherClassifierTestPred = classifierKNN.predict(X_test)
 npYtest = np.array(Y_test)
 print("K-Nearest Neighbour " + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
 
@@ -67,21 +79,11 @@ otherClassifierTestPred = classifierNB.predict(X_test_scaled)
 npYtest = np.array(Y_test)
 print("Gaussian NB" + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
 
-classifierNB = svm.LinearSVC()
-classifierNB.fit(X_train_scaled, Y_train)
-otherClassifierTestPred = classifierRndForest.predict(X_test_scaled)
-npYtest = np.array(Y_test)
-print("SVM " + " Test set score: {:.2f}".format(np.mean(otherClassifierTestPred == npYtest)))
-
 clf = DecisionTreeClassifier(random_state=42)
 
-# Train the classifier
 clf.fit(X_train_scaled, Y_train)
-
-# Predict on the test set
 Y_pred = clf.predict(X_test_scaled)
 
-# Evaluate the classifier
 print("Accuracy:", accuracy_score(Y_test, Y_pred))
 print("\nClassification Report:\n", classification_report(Y_test, Y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(Y_test, Y_pred))
